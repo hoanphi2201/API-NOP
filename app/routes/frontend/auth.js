d681199e3df39c26d788a49aed992990a3f1c9ae
Phi Xuan Hoan <36753744+hoanphi2201@users.noreply.github.com>
2019-05-11 14:26:44 +0700

    api đăng nhập fb

---
+++ app/routes/frontend/auth.js
@@ -0,0 +1,38 @@
+
+const usersModel = require(__pathSchemas + 'users');
+const randomstring = require("randomstring");
+const mailHelper = require(__pathHelper + 'mail');
+module.exports = function (app, passport) {
+    /*--------------------------------------------
+     |  auth đăng nhập local
+     --------------------------------------------*/
+     app.post('/auth/user/login', function (req, res, next) {    
+        passport.authenticate('local-login', function (err, user, info) {
+            if (err) {
+                return next(err);
+            }            
+            if (!user) {
+                return res.json({ error: 
+                    { message: 'Username or password is incorrect' }
+                });
+            }
+            req.logIn(user, function (err) {
+                
+                if (err) {
+                    return res.json({ error: 
+                        { message: 'Username or password is incorrect' }
+                    });
+                }
+                return res.json({
+                    status: 200,
+                    name: user.name,
+                    avatar: user.avatar,
+                    username: user.local.username,
+                    id: user.id,
+                });
+            });
+        })(req, res, next);
+    });
+    
+}
+    
