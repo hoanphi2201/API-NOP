2702888155158a965559b876a7f407fb0d6c7510
Phi Xuan Hoan <36753744+hoanphi2201@users.noreply.github.com>
2019-05-11 14:29:01 +0700

    api đăng nhập google

--- app/routes/frontend/auth.js
+++ app/routes/frontend/auth.js
@@ -62,5 +62,36 @@ module.exports = function (app, passport) {
             });
         })(req, res, next);
     });
+     /*--------------------------------------------
+     |  auth Đăng nhập GOOGLE
+     --------------------------------------------*/
+     app.post('/auth/user/login-google', function (req, res, next) {    
+        passport.authenticate('google-login', function (err, user, info) {
+            if (err) {
+                return next(err);
+            }            
+            if (!user) {
+                return res.json({ error: 
+                    { message: 'Username or password is incorrect' }
+                });
+            }
+            req.logIn(user, function (err) {
+                
+                if (err) {
+                    return res.json({ error: 
+                        { message: 'Username or password is incorrect' }
+                    });
+                }
+                return res.json({
+                    status: 200,
+                    name: user.name,
+                    avatar: user.avatar,
+                    username: user.local.username,
+                    id: user.id
+                });
+            });
+        })(req, res, next);
+    });
+    
 }
     
