d7e33413608ddeeb253931e961a603f8f1d9bd82
Phi Xuan Hoan <36753744+hoanphi2201@users.noreply.github.com>
2019-05-11 14:27:58 +0700

    api đăng nhập facebook

--- app/routes/frontend/auth.js
+++ app/routes/frontend/auth.js
@@ -33,6 +33,34 @@ module.exports = function (app, passport) {
             });
         })(req, res, next);
     });
-    
+    /*--------------------------------------------
+     |  auth đăng nhập FACEBOOK
+     --------------------------------------------*/
+     app.post('/auth/user/login-facebook', function (req, res, next) {    
+        passport.authenticate('facebook-login', function (err, user, info) {
+            if (err) {
+                return next(err);
+            }            
+            if (!user) {
+                return res.json({ error: 
+                    { message: 'Username or password is incorrect' }
+                });
+            }
+            req.logIn(user, function (err) {
+                if (err) {
+                    return res.json({ error: 
+                        { message: 'Username or password is incorrect' }
+                    });
+                }
+                return res.json({
+                    status: 200,
+                    name: user.name,
+                    avatar: user.avatar,
+                    username: user.local.username,
+                    id: user.id
+                });
+            });
+        })(req, res, next);
+    });
 }
     
